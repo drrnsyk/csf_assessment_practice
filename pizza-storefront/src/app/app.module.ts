8c8b5c9b8f395ee699504392c0e49108886e02d4
drrnsyk <darren.syk@gmail.com>
2023-02-23 00:32:09 +0800

    completed task1 and task2

--- pizza-storefront/src/app/app.module.ts
+++ pizza-storefront/src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main.component';
+import { OrdersComponent } from './components/orders.component';
+import { PizzaService } from './pizza.service';
 
+const appsRoute: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'orders/:email', component: OrdersComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+]
 
 @NgModule({
   declarations: [
-    AppComponent, MainComponent
+    AppComponent, MainComponent, OrdersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appsRoute, { useHash: true })
   ],
 
-  providers: [ ],
+  providers: [PizzaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- pizza-storefront/src/app/components/main.component.ts
+++ pizza-storefront/src/app/components/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Order } from '../models';
 
 const SIZES: string[] = [
   "Personal - 6 inches",
@@ -21,13 +24,60 @@ export class MainComponent implements OnInit {
 
   pizzaSize = SIZES[0]
 
-  constructor() {}
+  orderForm!: FormGroup
+
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
+    this.orderForm = this.createForm()
+    this.orderForm.reset()
   }
 
   updateSize(size: string) {
     this.pizzaSize = SIZES[parseInt(size)]
+  }
+
+  doPlaceOrder() {
+    const order: Order = this.orderForm.value as Order
+    console.info('>>> MainComponent: ngSubmit: order: ', order)
+    // navigate to the view 1
+    this.router.navigate(['/orders', order.email])
+  }
+
+  listOrders() {
+    const email: string = this.orderForm.value['email']
+    // navigate to the view 1
+    this.router.navigate(['/orders', email])
+  }
+
+  onCheckboxChange(e: any) {
+    const toppings: FormArray = this.orderForm.get('toppings') as FormArray;
+    if (e.target.checked) {
+      toppings.push(new FormControl(e.target.value));
+    } else {
+      let i: number = 0;
+      toppings.controls.forEach((item: AbstractControl) => {
+        if (item.value == e.target.value) {
+          toppings.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+  }
+
+
+  // helper functions
+  private createForm() {
+    return this.fb.group({
+      name: this.fb.control<string>('', Validators.required),
+      email: this.fb.control<string>('', [Validators.required, Validators.email]),
+      size: this.fb.control<number>(0, Validators.required),
+      base: this.fb.control<string>('', Validators.required),
+      sauce: this.fb.control<string>('', Validators.required),
+      toppings: this.fb.array([], [Validators.required, Validators.min(1)]),
+      comments: this.fb.control<string>(''),  
+    })
   }
 
 }
